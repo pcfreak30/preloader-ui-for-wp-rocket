17e60e972f679838c9ee713777d2525b92f2c4fa
Derrick Hammer <derrick@derrickhammer.com>
2019-10-29 20:08:32 -0400

    *Ensure the get_rocket_option function exists

--- preloader-ui-for-wp-rocket.php
+++ preloader-ui-for-wp-rocket.php
@@ -24,7 +24,7 @@ function preloader_ui_rocket_container( $item, $value = null ) {
 
 function preloader_ui_rocket_admin_menu() {
 	require_once __DIR__ . '/classes/class-preloader-ui-rocket-table.php';
-	if ( 0 < get_rocket_option( 'manual_preload' ) ) {
+	if ( function_exists( 'get_rocket_option' ) && 0 < get_rocket_option( 'manual_preload' ) ) {
 		add_management_page( 'Preload Queue', 'WP Rocket Preload Queue', 'manage_options', 'wp-rocket-preload-ui-full', 'preloader_ui_rocket_page_full' );
 		add_management_page( 'Post Preload Queue', 'WP Rocket Post Preload Queue', 'manage_options', 'wp-rocket-preload-ui-partial', 'preloader_ui_rocket_page_partial' );
 		require_once __DIR__ . '/classes/class-preloader-ui-rocket-table-web.php';
